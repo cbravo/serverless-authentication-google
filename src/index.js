d03c5d668e2cd009dbd31fa48e6b1887a17951e5
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-04-04 00:33:27 +0300

    removed commented code, added authorizationMethod param

--- src/index.js
+++ src/index.js
@@ -15,18 +15,11 @@ class GoogleProvider extends Provider {
     var options = {
       authorization_uri: 'https://www.googleapis.com/oauth2/v4/token',
       profile_uri: 'https://www.googleapis.com/plus/v1/people/me',
-      profileMap: mapProfile
+      profileMap: mapProfile,
+      authorizationMethod: 'POST'
     };
     super.callback(event, options, {grant_type: 'authorization_code'}, callback);
   }
-}
-
-export function signin(config, options, callback) {
-  (new GoogleProvider(config)).signin(options, callback);
-}
-
-export function callback(event, config, callback) {
-  (new GoogleProvider(config)).callback(event, callback);
 }
 
 function mapProfile(response) {
@@ -40,47 +33,10 @@ function mapProfile(response) {
   });
 }
 
-// function signin({id, redirect_uri}, {scope = 'profile', state}, callback) {
-//   let params = {
-//     client_id: id,
-//     redirect_uri,
-//     scope,
-//     response_type: 'code'
-//   };
-//
-//   if(state) {
-//     params.state = state;
-//   }
-//
-//   let url = utils.urlBuilder('https://accounts.google.com/o/oauth2/v2/auth', params);
-//   callback(null, {url});
-// }
-//
-// function callback({code, state}, {id, redirect_uri, secret}, callback) {
-//   async.waterfall([
-//     (callback) => {
-//       let payload = {
-//         client_id: id,
-//         redirect_uri,
-//         client_secret: secret,
-//         code,
-//         grant_type: 'authorization_code'
-//       };
-//       request.post('https://www.googleapis.com/oauth2/v4/token', {form: payload}, callback);
-//     },
-//     (response, accessData, callback) => {
-//       let {access_token} = JSON.parse(accessData);
-//       let url = utils.urlBuilder('https://www.googleapis.com/plus/v1/people/me', {access_token});
-//       request.get(url, (error, response, profileData) => {
-//         if(!error) {
-//           callback(null, mapProfile(JSON.parse(profileData)));
-//         } else {
-//           callback(err);
-//         }
-//       });
-//     }
-//   ], (err, data) => {
-//     callback(err, data, state);
-//   });
-// }
-//
+export function signin(config, options, callback) {
+  (new GoogleProvider(config)).signin(options, callback);
+}
+
+export function callback(event, config, callback) {
+  (new GoogleProvider(config)).callback(event, callback);
+}
